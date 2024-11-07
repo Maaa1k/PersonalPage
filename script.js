047d0da0c0270d4f54bae1ea95b10d2aebb1b419
Maaa1k <m.a.lopez.bosch@gmail.com>
2024-11-07 20:46:30 +0100

    big push

---
+++ script.js
@@ -0,0 +1,34 @@
+const links = document.querySelectorAll('.nav-link');
+
+function showSection(targetId) {
+    const allSections = document.querySelectorAll('.seccion');
+    allSections.forEach(section => section.classList.remove('activa'));
+
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+        targetSection.classList.add('activa');
+    }
+
+    const footer = document.querySelector('footer');
+    footer.classList.remove('visible');
+}
+
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        const targetId = link.getAttribute('data-target');
+
+        showSection(targetId);
+    });
+});
+
+window.addEventListener('scroll', function() {
+    const footer = document.querySelector('footer');
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    if (scrollPosition >= documentHeight - 50) {
+        footer.classList.add('visible');
+    } else {
+        footer.classList.remove('visible');
+    }
+});
